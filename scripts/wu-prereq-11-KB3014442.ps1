c525afef2f96aec3de5ff904a6da6cf530b20872
Jesse Boyce <jesse@jpboyce.org>
2018-10-07 11:25:15 +1000

    prereq scripts

---
+++ scripts/wu-prereq-11-KB3014442.ps1
@@ -0,0 +1,33 @@
+# Installing Windows Updates Prereqs
+Start-Transcript -Verbose
+$wusa = "$env:systemroot\System32\wusa.exe"
+$update = "x:\Windows8.1-KB3014442-x64.msu"
+$scriptname = $MyInvocation.MyCommand
+
+# Starting...
+Write-Output "[$($scriptname)] Starting execution..."
+
+Write-Output "[$($scriptname)] Checking if patch file is accessable..."
+if ((Test-Path $update)) {
+  Write-Output "[$($scriptname)] Patch file ($($update)) can be accessed"
+} else {
+  Write-Output "[$($scriptname)] Patch file ($($update)) can not be accessed, attempting to map drive"
+  NET USE X: \\Vboxsvr\2012r2patches
+}
+
+Write-Output "[$($scriptname)] Running patch command"
+# Start-Process -FilePath $wusa -ArgumentList ($update, '/quiet', '/log') -Verbose -Wait
+$patchrun = Start-Process -FilePath $wusa -ArgumentList ($update, '/quiet', '/log') -Verbose -PassThru
+
+for($i = 0; $i -le 100; $i = ($i + 1) % 100) {
+  Write-Progress -Activity "Installing $($update)" -PercentComplete $i -Status "Installing..."
+  start-sleep -Milliseconds 1000
+  if ($patchrun.HasExited) {
+    Write-Progress -Activity "Installing $($update)" -Completed
+    break
+  }
+}
+
+Write-Output "[$($scriptname)] Finished execution"
+
+Stop-Transcript -Verbose
