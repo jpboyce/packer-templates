e832a980b0b9af39834a2a2bf794fedb4ad75ca9
Jesse Boyce <jesse@jpboyce.org>
2019-01-08 05:17:05 +1000

    Installing guest additions via Powershell

---
+++ scripts/install-vbox-guest-additions.ps1
@@ -0,0 +1,43 @@
+# Install the VirtualBox Guest Additions using the ISO that's uploaded to the test
+
+# Setup variables
+$iso_path = "C:/users/vagrant/vBoxGuestAdditions.iso"
+# $iso_checksum = "1a56b8771c5a865f10b44b8d3b821e8404ea2f10978675e5cdc405525de7b5eb"
+# Updated checksum
+$iso_checksum = "e51e33500a265b5c2d7bb2d03d32208df880523dfcb1e2dde2c78a0e0daa0603"
+
+# Start transcript log
+Start-Transcript -Verbose
+
+# If the file doesn't exist, just loop for a while
+Write-Output "Starting checks for the ISO file..."
+while (!(Test-Path $iso_path)) {
+  Write-Output "ISO file doesn't exist, sleeping..."
+  Start-Sleep 10
+}
+
+# Check the checksum
+Write-Output "ISO File exists, checking the checksum..."
+$current_checksum = (Get-FileHash -Path $iso_path -Algorithm SHA256).hash.tolower()
+Write-Output "Current checksum is: $($current_checksum)"
+if ($current_checksum -eq $iso_checksum) {
+  Write-Output "The checksum matches what it should be, we can continue!"
+  Write-Output "Mounting the ISO as X: drive..."
+  Mount-DiskImage -ImagePath $iso_path -StorageType ISO -NoDriveLetter -Verbose
+  $drive = gwmi win32_volume -Filter "Label = 'VBox_GAs_5.2.22'"
+  $drive.DriveLetter = "X:"
+  $drive.Put()
+
+  Write-Output "Installing certificates..."
+  Get-ChildItem X:\cert -Filter *.cer | ForEach-Object { certutil -addstore -f "TrustedPublisher" $_.FullName }
+  Write-Debug "Finished adding certificates!"
+
+  Write-Output "Starting VBox additions installation..."
+  Start-Process -FilePath "X:\VBoxWindowsAdditions.exe" -ArgumentList "/S" -Wait
+  Write-Output "Finished installing!"
+} else {
+  Write-Output "The checksum value did not match..."
+}
+
+# Stop transcript log
+Stop-Transcript -Verbose
