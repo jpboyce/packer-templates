8269fc0c7767a76787c0bf29c75cb17f63d2562c
Jesse <jesse@jpboyce.org>
2019-05-26 04:51:24 +1000

    Commented out restart commands

--- scripts/windows/windowsupdates.ps1
+++ scripts/windows/windowsupdates.ps1
@@ -30,10 +30,10 @@ function Check-ContinueRestartOrEnd() {
                 Install-WindowsUpdates
             } elseif ($script:Cycles -gt $global:MaxCycles) {
                 LogWrite "Exceeded Cycle Count - Stopping"
-                Invoke-Expression "a:\openssh.ps1 -AutoStart"
+                # Invoke-Expression "a:\openssh.ps1 -AutoStart"
             } else {
                 LogWrite "Done Installing Windows Updates"
-                Invoke-Expression "a:\openssh.ps1 -AutoStart"
+                # Invoke-Expression "a:\openssh.ps1 -AutoStart"
             }
         }
         1 {
@@ -46,7 +46,7 @@ function Check-ContinueRestartOrEnd() {
             }
 
             LogWrite "Restart Required - Restarting..."
-            Restart-Computer -Force
+            # Restart-Computer -Force
         }
         default {
             LogWrite "Unsure If A Restart Is Required"
@@ -206,7 +206,7 @@ function Check-WindowsUpdates() {
             $global:MoreUpdates=0
             Check-ContinueRestartOrEnd
             LogWrite "Show never happen to see this text!"
-            Restart-Computer -Force
+            # Restart-Computer -Force
         }
     } else {
         LogWrite 'There are no applicable updates'
