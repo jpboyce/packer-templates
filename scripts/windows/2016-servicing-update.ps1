8c7daa539ac9a34d8b21c8f9776d3a3b27e9eabc
Jesse <jesse@jpboyce.org>
2020-05-03 08:22:14 +1000

    Updated path

--- scripts/windows/2016-servicing-update.ps1
+++ scripts/windows/2016-servicing-update.ps1
@@ -4,8 +4,9 @@
 Start-Transcript -Verbose
 
 # Download the file
-$fileUrl = "http://$($env:HTTP_IP):$($env:HTTP_PORT)/Microsoft/Windows-2016/kb4498947/windows10.0-kb4498947-x64_97b6d1b006cd564854f39739d4fc23e3a72373d7.msu"
-$localFilePath = Join-Path -Path $env:TEMP -ChildPath "windows10.0-kb4498947-x64_97b6d1b006cd564854f39739d4fc23e3a72373d7.msu"
+# $fileUrl = "http://$($env:HTTP_IP):$($env:HTTP_PORT)/Microsoft/Windows-2016/kb4498947/windows10.0-kb4498947-x64_97b6d1b006cd564854f39739d4fc23e3a72373d7.msu"
+$fileUrl = "http://192.168.20.2/software/Microsoft/Windows-2016/servicing-stack-updates/2019-07/windows10.0-kb4509091-x64_1e1039047044a1f632562cd65df3ce6b77804018.msu"
+$localFilePath = Join-Path -Path $env:TEMP -ChildPath "windows10.0-kb4509091-x64_1e1039047044a1f632562cd65df3ce6b77804018.msu"
 Write-Output "Using file URL of: $fileUrl"
 Write-Output "Saving file to: $localFilePath"
 
