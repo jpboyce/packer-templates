6fa1b147d2330c8385e5a9b6b7b393221ef5836c
Jesse <jesse@jpboyce.org>
2020-05-05 04:23:04 +1000

    yum update, removed kernel-dev

--- scripts/centos/vbox-prereqs.sh
+++ scripts/centos/vbox-prereqs.sh
@@ -11,10 +11,21 @@ fi
 echo "Enabling the EPEL repository..."
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
+# Install YUM utils so we get config-manager
+echo "Installing YUM utils..."
+sudo yum -y install yum-utils
+
+# Run an update first
+echo "Running a YUM Update..."
+sudo yum -y update
+
 # Install prerequisite items
 echo "Installing prerequisite items via YUM..."
 sudo yum -y install perl gcc dkms kernel-devel kernel-headers make bzip2
-sudo yum -y install kernel-devel-`uname -r`
+echo "Enable CR Repo"
+sudo yum-config-manager --enable cr -y
+#echo "Installing kernel-devel"
+#sudo yum -y install kernel-devel-`uname -r`
 
 # Add KERN_DIR environment variable
 echo "Setting KERN_DIR environment variable..."
